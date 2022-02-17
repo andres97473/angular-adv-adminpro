90b0ae3f071237977a6c8e55aeb5fce6302a3f07
andres97473 <andres97473@hotmail.com>
2022-02-17 18:33:43 -0500

    Fin de seccion 7

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [BreadcrumsComponent, SidebarComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [BreadcrumsComponent, SidebarComponent, HeaderComponent],
 })
 export class SharedModule {}
